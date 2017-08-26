e8bb966b169cbbffe1160919298da1a3ee8cb424
Dean Norman <dean@22seven.com>
2017-08-26 15:34:18 +0200

    added and created a test for util functions

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { partial } from '../lib/utils'
 
 const TodoListItem = (props) => {
+  // const handleToggle = props.handleToggle.bind(null, props.id);
+  const handleToggle = partial(props.handleToggle, props.id);
   return(
     <li key={props.id}>
       <input type="checkbox"
-        onChange={() => props.handleToggle(props.id)}
+        onChange={handleToggle}
         defaultChecked={props.isComplete}/> {props.name}
     </li>
   )
