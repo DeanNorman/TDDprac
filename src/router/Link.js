8c796504f5c5d25b991ad8a88393b90c26f7751a
Dean Norman <dean@22seven.com>
2017-09-06 14:31:25 +0200

    created custom router

--- src/router/Link.js
+++ src/router/Link.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Link extends Component {
+    static contextTypes = {
+        route: PropTypes.string,
+        linkHandler: PropTypes.func,
+    }
+     
     handleClick = (event) => {
         event.preventDefault() // eslint-disable-line
-        window.history.pushState(null, '', this.props.to)
+        this.context.linkHandler(this.props.to)
     }
+
     render() {
+        const activeClass = this.context.route === this.props.to ? 'active' : '';
         return ( // eslint-disable-next-line
-            <a href="#" onClick={this.handleClick}>{this.props.children}</a>
+            <a href="#" className={activeClass} onClick={this.handleClick}>{this.props.children}</a>
         )
     }
 }
