b3d9c0e2416438ec45add4d87339e578d61603b7
Dean Norman <dean@22seven.com>
2017-09-07 13:50:16 +0200

    Added put and delete calls to todo service

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import { TodoForm,TodoList,Footer } from './components/index';
 import { addTodo, newId, findById, toggleTodo, updateTodo, removeTodo, filterTodos } from './lib/TodoHelpers';
 import { pipe, partial } from './lib/utils';
-import { loadTodos, createTodo } from './lib/todoService';
+import { loadTodos, createTodo, saveTodo, destroyTodo } from './lib/todoService';
 
 class App extends Component {
   state = {
@@ -30,14 +30,18 @@ class App extends Component {
     event.preventDefault()
     const updatedTodos = removeTodo(this.state.todos, id)
     this.setState({ todos: updatedTodos})
+    destroyTodo(id).then(() => this.showTempMessage('Todo Deleted'))
   }
 
   handleToggle = (id) => {
-    const getUpdatedTodos = pipe(findById, toggleTodo, partial(updateTodo, this.state.todos))
-    const updatedTodos = getUpdatedTodos(id, this.state.todos)
+    const getToggledTodo = pipe(findById, toggleTodo)
+    const updated = getToggledTodo(id, this.state.todos)
+    const getUpdatedTodos = partial(updateTodo, this.state.todos)
+    const updatedTodos = getUpdatedTodos(updated)
     this.setState({
       todos: updatedTodos,
     })
+    saveTodo(updated).then(() => this.showTempMessage('Todo Updated'))
   }
 
   handleSubmit = (event) => {
